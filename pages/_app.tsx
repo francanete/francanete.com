80b63f08e9e31a4da3d50689c8786e21c4146df2
Fran Canete <francanetedev@gmail.com>
2022-02-05 19:19:14 +0000

    Fix dark theme rendering bug

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import { AppProps } from 'next/app'
-import '../styles/index.css'
+import { AppProps } from "next/app";
+import React, { useEffect, useState } from "react";
+import useDarkMode from "../hooks/useDarkMode";
+import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [colorTheme, setTheme]: any = useDarkMode();
+  const [navbarOpen, setNavbarOpen] = React.useState(false);
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
+  return <Component {...pageProps} />;
 }
