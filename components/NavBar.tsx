de600c542c6b33c95ce87d415a3269e1f6c9db2b
Fran Canete <francanetedev@gmail.com>
2022-02-12 17:20:40 +0000

    Create new projects page

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -81,10 +81,10 @@ export default function NavBar() {
             </li>
             <li>
               <a
-                href="#"
+                href="/projects"
                 className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Services
+                Projects
               </a>
             </li>
             <li>
--- pages/projects.tsx
+++ pages/projects.tsx
@@ -3,6 +3,7 @@ import Post from "../types/post";
 import MoreStories from "../components/more-stories";
 import Container from "../components/container";
 import { getAllProjects } from "../lib/apiProjects";
+import ProjectPreview from "../components/project-preview";
 
 type Props = {
   allProjects: Post[];
@@ -12,7 +13,25 @@ export default function Blog({ allProjects }: Props) {
   return (
     <Layout>
       <Container>
-        <MoreStories posts={allProjects} />
+        {/* <MoreStories posts={allProjects} /> */}
+        <section>
+          <h2 className="mb-8 text-3xl md:text-3xl font-bold tracking-tighter leading-tight">
+            Projects
+          </h2>
+          <div className="grid grid-cols-1 md:grid-cols-1 mx-40 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+            {allProjects.map((project) => (
+              <ProjectPreview
+                key={project.slug}
+                title={project.title}
+                coverImage={project.coverImage}
+                date={project.date}
+                author={project.author}
+                slug={project.slug}
+                excerpt={project.excerpt}
+              />
+            ))}
+          </div>
+        </section>
       </Container>
     </Layout>
   );
