718cbeae66c229ee9b641b55d24683fc46632084
Fran Canete <francanetedev@gmail.com>
2022-02-17 08:24:06 +0000

    Initial commit

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -23,7 +23,7 @@ const HeroPost = ({
 }: Props) => {
   return (
     <section>
-      <div className="mb-8 md:mb-16 ">
+      <div className="mb-8 md:mb-16">
         <CoverImage title={title} src={coverImage} slug={slug} />
       </div>
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
