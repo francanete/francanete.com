e4475d70ba46c0eb23419e082028b3e9ce577b50
Fran Canete <francanetedev@gmail.com>
2022-03-29 20:02:46 +0100

    Refactoring components

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { TypographyProps } from "../types/TypographyProps";
 
-interface ButtonProps {
+interface ButtonProps extends Omit<TypographyProps, "level"> {
   title: string;
   iconComponent?: React.ReactNode;
 }
 
-export const Button = ({ title, iconComponent }: ButtonProps) => {
+export const Button = ({
+  title,
+  iconComponent,
+  marginTop,
+  marginBottom,
+}: ButtonProps) => {
   return (
     <button
       type="button"
-      className="group text-white text-xs md:text-sm bg-dark focus:ring-4 focus:outline-none focus:ring-slate-400/40 rounded-lg px-5  inline-flex items-center dark:text-slate-900  dark:bg-slate-200 dark:hover:bg-slate-300 dark:focus:ring-slate-100/30"
+      className={`group mt-${marginTop} mb-${marginBottom} text-white text-xs md:text-sm bg-dark focus:ring-4 focus:outline-none focus:ring-slate-400/40 rounded-lg px-5  inline-flex items-center dark:text-slate-900  dark:bg-slate-200 dark:hover:bg-slate-300 dark:focus:ring-slate-100/30`}
     >
       {iconComponent}
       {title}
--- components/ProjectHeader.tsx
+++ components/ProjectHeader.tsx
@@ -26,10 +26,14 @@ export const ProjectHeader = ({
 }: Props) => {
   return (
     <div className="max-w-2xl mx-auto py-10 md:py-24 flex flex-col">
-      <PostTitle weight="bold" marginBottom="6" size="4xl" level="h1">
+      <PostTitle weight="bold" marginBottom="4" size="4xl" level="h1">
         {title}
       </PostTitle>
-      <ProjectTechnologies technologies={technologies} />
+      <ProjectTechnologies
+        marginTop="4"
+        marginBottom="3"
+        technologies={technologies}
+      />
       <div className="mx-w-2xl mx-auto">
         <p className="leading-relaxed text-base mx-w-2xl mx-auto font-extralight">
           {excerpt}
