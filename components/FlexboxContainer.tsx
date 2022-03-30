b84d20b44c83109eb412af6c11c37ca08a9a16a6
Fran Canete <francanetedev@gmail.com>
2022-03-30 21:09:52 +0100

    Fix FlexboxContainer styling props

--- components/FlexboxContainer.tsx
+++ components/FlexboxContainer.tsx
@@ -4,8 +4,22 @@ import { BoxModelProps } from "../types/BoxModelProps";
 interface Props extends BoxModelProps {
   children?: ReactNode;
   display?: "flex";
-  justifyContent?: "start" | "end" | "center" | "between" | "around" | "evenly";
-  gap?: "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8";
+  justifyContent?:
+    | "justify-start"
+    | "justify-end"
+    | "justify-center"
+    | "justify-between"
+    | "justify-around"
+    | "justify-evenly";
+  gap?:
+    | "gap-1"
+    | "gap-2"
+    | "gap-3"
+    | "gap-4"
+    | "gap-5"
+    | "gap-6"
+    | "gap-7"
+    | "gap-8";
   tailwind?: string;
 }
 
@@ -24,9 +38,7 @@ export const FlexboxContainer = ({
   tailwind,
 }: Props) => {
   return (
-    <div
-      className={`${tailwind} ${display} justify-${justifyContent} gap-${gap} mt-${marginTop} pt-${paddingTop} mb-${marginBottom}  mx-${marginHorizontal} my-${marginVertical} px-${paddingHorizontal} py-${paddingVertical} `}
-    >
+    <div className={`${tailwind} ${display} ${justifyContent} ${gap}`}>
       {children}
     </div>
   );
