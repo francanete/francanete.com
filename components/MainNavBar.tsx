be5af13671909fc15995d116289642e48f3787c9
francanete <francanetedev@gmail.com>
2022-11-26 12:05:30 +0000

    refactor navbar

--- components/MainNavBar.tsx
+++ components/MainNavBar.tsx
@@ -2,6 +2,12 @@ import Link from "next/link";
 import React from "react";
 import { useRouter } from "next/router";
 import useDarkMode from "../hooks/useDarkMode";
+import { menu } from "../utils/getMenuItems";
+import { BiMenu } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+
+import styles from "./MainNavBar.module.scss";
+import classNames from "classnames";
 
 export default function MainNavBar() {
   const [colorTheme, setTheme]: any = useDarkMode();
@@ -9,78 +15,42 @@ export default function MainNavBar() {
   const currentPath = useRouter();
   const isRootPath = currentPath.pathname === "/tempo";
 
-  const menu = [
-    { title: "Home", path: "/" },
-    { title: "About", path: "/about" },
-    { title: "Blog", path: "/blog" },
-    { title: "Projects", path: "/projects" },
-  ];
-
   const showMenu = isRootPath ? menu.filter((item) => item.path !== "/") : menu;
 
   return (
-    <nav className="px-2 sm:px-4 py-5">
+    <nav className={styles["MainNavBar"]}>
       <div
-        className={`container flex flex-wrap ${
-          isRootPath ? "md:justify-center" : "md:justify-between"
-        } justify-between  items-center mx-auto`}
+        className={classNames(styles["MainNavBar__container"], {
+          [styles["MainNavBar__container--isRootPath"]]: isRootPath,
+        })}
       >
         {!isRootPath ? (
           <Link href="/" className="flex ">
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-              Fran Canete
-            </span>
+            <span className={styles["MainNavBar__logo"]}>Fran Canete</span>
           </Link>
-        ) : (
-          <span></span>
-        )}
-
+        ) : null}
         <button
           onClick={() => setNavbarOpen(!navbarOpen)}
           data-collapse-toggle="mobile-menu "
           type="button"
-          className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          className={styles["MainNavBar__hamburger"]}
           aria-controls="mobile-menu-2"
           aria-expanded="false"
         >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-          <svg
-            className="hidden w-6 h-6"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              fillRule="evenodd"
-              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
+          {navbarOpen ? <BiMenu size={30} /> : <AiOutlineClose size={30} />}
         </button>
         <div
-          className={
-            "w-full md:block md:w-auto" + (navbarOpen ? "" : " hidden")
-          }
+          className={classNames(styles["MainNavBar__menuItems"], {
+            [styles["MainNavBar__menuItems--hidden"]]: navbarOpen,
+          })}
         >
-          <ul className="flex flex-col items-center mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium ">
+          <ul className={styles["MainNavBar__itemsList"]}>
             {showMenu.map((item) => {
               return (
                 <li key={item.path}>
                   <Link
                     href={item.path}
-                    className="block py-2 pr-4 pl-3 text-gray-700 bg-grey-900 rounded md:bg-transparent md:text-black hover:underline  md:p-0 dark:text-white "
+                    className={styles["MainNavBar__item"]}
                     aria-current="page"
                   >
                     {item.title}
@@ -89,7 +59,7 @@ export default function MainNavBar() {
               );
             })}
 
-            {!navbarOpen ? (
+            {/* {navbarOpen ? (
               <>
                 <span onClick={() => setTheme(colorTheme)}>
                   {colorTheme === "light" ? (
@@ -148,7 +118,7 @@ export default function MainNavBar() {
                   </>
                 )}
               </span>
-            )}
+            )} */}
           </ul>
         </div>
       </div>
