fee9885067100a63364324867a7be7b856eefd9a
francanete <francanetedev@gmail.com>
2023-01-22 12:50:16 +0000

    add sitemap file

--- apps/francanete/next.config.js
+++ apps/francanete/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  siteUrl: process.env.SITE_URL || "https://francanete.com",
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
