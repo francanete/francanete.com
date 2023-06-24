24644097abc25b45f24433718eb9b4c98e0a7231
francanete <francanetedev@gmail.com>
2023-06-24 14:20:05 +0100

    Updates articles view

--- apps/francanete/components/BlogPageHeader.tsx
+++ apps/francanete/components/BlogPageHeader.tsx
@@ -4,7 +4,7 @@ import { PageHeader } from "./PageHeader";
 export const BlogPageHeader = () => {
   return (
     <PageHeader
-      title="Check out my latest articles"
+      title="Check out my latest articles:"
       description="From software engineering to entrepreneurship, I write about a variety
         of topics. Here are some of my latest articles:"
     />
