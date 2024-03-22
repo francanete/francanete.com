801c3ddd5d6acc9c6855f50a6cb795df26bb851b
francanete <francanetedev@gmail.com>
2024-03-22 18:22:52 +0000

    Remove turborepo
    Update env

---
+++ components/NewsletterForm.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import subscribeToNewsletter from "../utils/subscribeToNewsletter";
+import styles from "./NewsletterForm.module.scss";
+import { Heading } from "./Heading";
+
+export const NewsletterForm: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState({ type: "", content: "" });
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setMessage({ type: "", content: "" });
+    const result = await subscribeToNewsletter(email);
+    if (result.status === "success") {
+      setMessage({ type: "success", content: "Thank you for subscribing!" });
+      setEmail("");
+    } else {
+      setMessage({
+        type: "error",
+        content: `Failed to subscribe: ${result.message}`,
+      });
+    }
+  };
+
+  return (
+    <div className={styles["NewsletterForm"]}>
+      <Heading level={3} className={styles["NewsletterForm__header"]}>
+        Subscribe to my privarte email list
+      </Heading>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="Your email address"
+          className={styles["NewsletterForm__input"]}
+          required
+        />
+        <button type="submit" className={styles["NewsletterForm__button"]}>
+          Subscribe
+        </button>
+      </form>
+      {message.content && (
+        <div
+          className={`${styles["NewsletterForm__message"]} ${
+            styles[`NewsletterForm__message--${message.type}`]
+          }`}
+        >
+          {message.content}
+        </div>
+      )}
+    </div>
+  );
+};
